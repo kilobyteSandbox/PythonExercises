a87ca3adc7d1fabad2c94b54c2ec51361ce280f5
KoreyBulloch <a-koreyb@hotmail.com>
2015-10-20 18:40:55 -0700

    Added error checking and support for numbers with commas

--- 02collatz.py
+++ 02collatz.py
@@ -15,30 +15,40 @@ def collatzIntro():
 	print(" ")
 
 
-# Pick number and check to make sure input is a number
+# Have the user pick a number, and verify the number.
 def pickNumber():
 	while True:
 		try:
-			number = int(input())
-			return number
+			# Pick a number, remove commas, and convert to int.
+			number = int(input().replace(",", ""))
+			# Make sure it's less than 12 digits.
+			if len(str(number)) < 12:
+				return number
+			else:
+				print("Sorry, try a number under 1,000,000,000,000.")
+				continue
+		# If it's not a number, give friendly error and repeat.
 		except ValueError:
-			print("This requires a whole number (no commas or decimals).  Please try again.")
+			print("This requires a whole number (no decimals).  Please try again.")
 			print(" ")
 
 
 # Would you like to rerun the program?
-# Currently bugged.  All answers rerun.
 def rerun(answer, function):
 	if answer in ("Yes", "yes", "y"):
-		function
+		function()
 	else:
 		print("See you next time!")
 
 
-# Take number.  If 1, end.  If even, divide by 2.  If odd, multiply by 3 and add 1.
+# Run Collatz: Take number.  If 1, end.  If even, divide by 2.  If odd, multiply by 3 and add 1.
+# Print victory message, and ask if they'd like to try again.
 def collatz():
+	# Print intro
 	collatzIntro()
+	# Get number from user
 	num = pickNumber()
+	# Run Collatz formula and start counter
 	counter = 0
 	while num != 1:
 		if num // 2 == num /2:
@@ -47,11 +57,13 @@ def collatz():
 			num = int(num * 3 + 1)
 		print(num)
 		counter += 1
+	# Print victory message and ask player if they want to try again.
 	print("Huzzah!  It only took " + str(counter) + " operations.  Would you like to " +\
 		"try again? (Yes / No)")
 	response = input()
 	print(" ")
 	print(" ")
-	rerun(response, collatz())
+	# Rerun or exit based on user response.
+	rerun(response, collatz)
 
 collatz()
